159e10275f22e8adb41306d6edabb12a1c77fcb5
Manny Serrano <158786148+manny-serrano@users.noreply.github.com>
2026-04-21 12:47:06 -0400

    Add: two-integer-sum - submission-16

---
+++ Data Structures & Algorithms/two-integer-sum/submission-16.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        found = {}
+
+        for i, value in enumerate(nums):
+            difference = target - value
+            if difference in found:
+                return [found[difference], i]
+            found[value] = i 
+        
+
+        
+       
